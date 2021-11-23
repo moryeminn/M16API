4130602a60771bcffdd20b66df46289f969b23d2
moryeminn <94890377+moryeminn@users.noreply.github.com>
2021-11-23 20:41:35 +0200

    Add files via upload

---
+++ M16AlignAPI/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using M16AlignAPI.Models;
+using M16AlignAPI.Service;
+
+namespace M16AlignAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // requires using Microsoft.Extensions.Options
+            services.Configure<MissionsDatabaseSettings>(
+                Configuration.GetSection(nameof(MissionsDatabaseSettings)));
+
+            services.AddSingleton<IMissionsDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<MissionsDatabaseSettings>>().Value);
+
+            services.AddSingleton<MissionService>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
